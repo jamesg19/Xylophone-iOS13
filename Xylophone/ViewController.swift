f1af7dcd39eaabd0bed6fc5ccdc8dfb834941d5b
jamesg19 <48737452+jamesg19@users.noreply.github.com>
2022-11-30 22:27:26 -0500

    Finish xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
+    var player: AVAudioPlayer?
 
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Welcome...")
+    }
 
+    
+    
+    @IBAction func cButton(_ sender: UIButton) {
+        playSound(nameSound:"C")
+    }
+    
+    @IBAction func dButton(_ sender: UIButton) {
+        print("dButton\n")
+        playSound(nameSound:"D")
+    }
+    @IBAction func eButton(_ sender: UIButton) {
+        playSound(nameSound:"E")
+    }
+    
+    @IBAction func fButton(_ sender: UIButton) {
+        playSound(nameSound:"F")
+    }
+    @IBAction func gButton(_ sender: UIButton) {
+        playSound(nameSound:"G")
+    }
+    
+    @IBAction func aButton(_ sender: UIButton) {
+        playSound(nameSound:"A")
+    }
+    
+    @IBAction func bButton(_ sender: UIButton) {
+        playSound(nameSound:"B")
+    }
+    
+    func playSound(nameSound:String) -> Void{ // need to declare local
+        print(nameSound)
+        //self.player?.stop()
+        let url = Bundle.main.url(forResource: nameSound, withExtension: "wav")
+        self.player = try! AVAudioPlayer(contentsOf: url!)
+
+        self.player!.prepareToPlay()
+        self.player!.play()
+
+
+    }
 }
 
